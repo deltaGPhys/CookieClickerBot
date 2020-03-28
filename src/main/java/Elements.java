93574a5164cc190947b87a5b820430a46cd9398f
deltaGPhys <cty.jgates@gmail.com>
2020-03-28 18:27:01 -0400

    classes - definitions

---
+++ src/main/java/Elements.java
@@ -0,0 +1,9 @@
+import lombok.Getter;
+import org.openqa.selenium.By;
+
+@Getter
+public class Elements {
+
+    private static final By byBigCookie = By.id("bigCookie");
+
+}
---
+++ src/main/java/URLDefinitions.java
@@ -0,0 +1,8 @@
+import lombok.Getter;
+
+@Getter
+public class URLDefinitions {
+
+    public static final String cookieClickerURL = "https://orteil.dashnet.org/cookieclicker/";
+
+}
